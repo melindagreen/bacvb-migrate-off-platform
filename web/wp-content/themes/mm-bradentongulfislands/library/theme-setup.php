c230d419ce724cb825a5e1b2787296ce42e83fa4
Aaron Fields <afields@maddenmedia.cm>
2023-05-16 09:07:47 -0700

    portal changes

--- web/wp-content/themes/mm-bradentongulfislands/library/theme-setup.php
+++ web/wp-content/themes/mm-bradentongulfislands/library/theme-setup.php
@@ -7,6 +7,7 @@ namespace MaddenNino\Library;
 class ThemeSetup {
 	function __construct () {
 		add_action( 'after_setup_theme', array( get_called_class(), 'madden_theme_support' ) );
+		add_action( 'init', 'add_partner_role' );
 	}
 
 	public static function madden_theme_support() {
@@ -28,4 +29,18 @@ class ThemeSetup {
 		) );
 	}
 
+	function add_partner_role() {
+		// Get the contributor role object
+		$contributor = get_role( 'contributor' );
+		
+		// Create the partner role as a copy of the contributor role
+		$partner = add_role(
+		  'partner',
+		  __( 'Partner', 'textdomain' ),
+		  $contributor->capabilities // use the same capabilities as the contributor role
+		);
+		
+		// Add the 'edit_published_posts' capability to the partner role
+		$partner->add_cap( 'edit_published_posts' );
+	  }  
 }
