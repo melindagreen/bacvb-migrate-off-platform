a4ee8a47ba1fe25d06312c62edf7d7f3334adcfe
Aaron Fields <afields@maddenmedia.cm>
2023-05-18 10:02:44 -0700

    amenities checkbox

--- web/wp-content/themes/mm-bradentongulfislands/library/theme-setup.php
+++ web/wp-content/themes/mm-bradentongulfislands/library/theme-setup.php
@@ -7,7 +7,40 @@ namespace MaddenNino\Library;
 class ThemeSetup {
 	function __construct () {
 		add_action( 'after_setup_theme', array( get_called_class(), 'madden_theme_support' ) );
-		add_action( 'init', array( get_called_class('add_partner_role')) );
+		
+		// Add 'partner' role with contributor capabilities
+add_role('partner', 'Partner', get_role('contributor')->capabilities);
+
+// Allow partners to edit their own posts
+add_action('init', 'allow_partner_edit_posts');
+function allow_partner_edit_posts() {
+    $partner_role = get_role('partner');
+    $partner_role->add_cap('edit_posts');
+    $partner_role->add_cap('edit_published_posts');
+}
+
+// Set partner posts to pending review after edit
+add_action('pre_post_update', 'set_partner_posts_pending_review', 10, 2);
+function set_partner_posts_pending_review($new_post, $old_post) {
+    if ($new_post->post_type === 'post' && current_user_can('partner') && $new_post->post_status === 'publish') {
+        $new_post->post_status = 'pending';
+        $new_post->post_date = $old_post->post_date;
+        $new_post->post_date_gmt = $old_post->post_date_gmt;
+        $new_post->post_modified = $old_post->post_modified;
+        $new_post->post_modified_gmt = $old_post->post_modified_gmt;
+        $new_post->post_name = $old_post->post_name;
+        $new_post->post_title = $old_post->post_title;
+        $new_post->post_content = $old_post->post_content;
+        $new_post->post_excerpt = $old_post->post_excerpt;
+        $new_post->post_password = $old_post->post_password;
+        $new_post->post_parent = $old_post->post_parent;
+        $new_post->post_mime_type = $old_post->post_mime_type;
+        $new_post->guid = $old_post->guid;
+        $new_post->menu_order = $old_post->menu_order;
+        $new_post->post_category = $old_post->post_category;
+    }
+}
+
 	}
 
 	public static function madden_theme_support() {
@@ -28,19 +61,4 @@ class ThemeSetup {
 			'flex-height' => true,
 		) );
 	}
-
-	public static function add_partner_role() {
-		// Get the contributor role object
-		$contributor = get_role( 'contributor' );
-		
-		// Create the partner role as a copy of the contributor role
-		$partner = add_role(
-		  'partner',
-		  __( 'Partner', 'textdomain' ),
-		  $contributor->capabilities // use the same capabilities as the contributor role
-		);
-		
-		// Add the 'edit_published_posts' capability to the partner role
-		$partner->add_cap( 'edit_published_posts' );
-	  }  
 }
