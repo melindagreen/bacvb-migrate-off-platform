b79cbe958bc421509370dd319d009b04096f862b
Aaron Fields <afields@maddenmedia.cm>
2023-08-29 12:44:36 -0700

    feat: block styling/settings changes

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/filters/custom-core-controls/constants.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/filters/custom-core-controls/constants.js
@@ -3,5 +3,6 @@
 // Controls available customizations
 export const CUSTOMIZE_BLOCKS = {
 	'core/columns': [ 'justify-content', 'overlap'],
+	'core/group': [ 'overlap', ],
 	'core/cover': [ 'wraparound-link', ],
 };
