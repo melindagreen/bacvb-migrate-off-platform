3a79d1798c51f4beaae60377451fd8dda6742317
ben <treeslikeme@gmail.com>
2023-12-13 16:49:38 -0800

    404 stories block

--- web/wp-content/themes/mm-bradentongulfislands/404.php
+++ web/wp-content/themes/mm-bradentongulfislands/404.php
@@ -8,9 +8,9 @@ get_header(); ?>
 	<div class="content-inner">
 		<h1>Oops!<span>Looks like this page<br>went to the beach.</span></h1>
 		<a class="button" href="<?php echo home_url(); ?>">Bring me back home <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/blue-arrow.png" alt="Go Home" /></a>
-		
 	</div>
-
 </div>
-
+<div>
+<?php echo do_blocks('<!-- wp:block {"ref":3991} /-->'); ?>
+</div>
 <?php get_footer(); ?>
