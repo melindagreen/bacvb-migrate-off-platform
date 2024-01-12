823090ab8afaa5158ac2b4550a31ac3e27c152ac
Amy Jen <ajen@maddenmedia.com>
2024-01-12 11:37:54 -0700

    updated mobile view to quick links block

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/quick-links/front.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/quick-links/front.js
@@ -9,21 +9,41 @@
       let isTucked = false;
       let lastScrollY = $(window).scrollTop();
 
-      $(window).scroll(function() {
-        const currentScrollY = $(window).scrollTop();
 
-        if (currentScrollY > lastScrollY && !isTucked) {
-          // Scrolling down, hide the element
-          element.addClass('tuck');
-          isTucked = true;
-        } else if (currentScrollY < lastScrollY && isTucked) {
-          // Scrolling up, show the element
-          element.removeClass('tuck');
-          isTucked = false;
-        }
+      if($(window).width() > 768) {
+        $(window).scroll(function() {
+          const currentScrollY = $(window).scrollTop();
 
-        lastScrollY = currentScrollY;
-      });
+          if (currentScrollY > lastScrollY && !isTucked) {
+            // Scrolling down, hide the element
+            element.addClass('tuck');
+            isTucked = true;
+          } else if (currentScrollY < lastScrollY && isTucked) {
+            // Scrolling up, show the element
+            element.removeClass('tuck');
+            isTucked = false;
+          }
+
+          lastScrollY = currentScrollY;
+        });
+
+      } else {
+
+        element.find('.quick-links-heading').click(function(){
+          element.find('.quick-link-items').slideToggle();
+        });
+
+        var stickyTop = element.offset().top - 88;
+
+        $(window).scroll(function() {
+          var windowTop = $(window).scrollTop();
+          if (stickyTop < windowTop) {
+            element.addClass('fixed-scroll');
+          } else {
+            element.removeClass('fixed-scroll');
+          }
+        });
+      }
 
     });
 })(jQuery);
