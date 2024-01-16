82cd94414e38b439bd9f432f5ca7924824b154b6
Aaron Fields <afields@maddenmedia.cm>
2024-01-16 15:03:48 -0800

    feat: showcase hero animation

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/hero-showcase/front.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/hero-showcase/front.js
@@ -1,18 +1,27 @@
+
+
 // this is the front-end script for the block example-dynamic
 
 (function($) {
-    $(document).ready(function() {
-        
+  $(document).ready(function() {
+      
+    const heroShowcaseElements = $(".hero-showcase");
 
+      $(heroShowcaseElements).each(function (index, element) {
+        console.log('Test');
+ 
+          const observer = new IntersectionObserver(entries => {
+              entries.forEach(entry => {
+                  if (entry.isIntersecting) {
+                      $(entry.target).css({
+                          opacity: 1,
+                          transform: "translateX(0px)"
+                      });
+                  }
+              });
+          }, { threshold: 0.1 });
 
-            var contentHeight = $(this).height();
-            var containerHeight = $(".grid-item-body").height();
-          
-            if (contentHeight > containerHeight) {
-              var newTop = contentHeight - containerHeight;
-              $(this).css("top", -newTop + "px");
-            } else {
-              $(this).css("top", "0");
-            }
-    });
+          observer.observe(element);
+      });
+  });
 })(jQuery);
