fa929bbcd001db3f54be25122ee4bfe65d38410c
Aaron Fields <afields@maddenmedia.cm>
2024-02-07 03:11:57 -0800

    feat: events partner portal cards

--- web/wp-content/themes/mm-bradentongulfislands/library/memberpress/nav-tabs.php
+++ web/wp-content/themes/mm-bradentongulfislands/library/memberpress/nav-tabs.php
@@ -18,7 +18,7 @@ class NavTabs {
             C::THEME_PREFIX . "-memberpress-account-css", // handle
             get_stylesheet_directory_uri()."/assets/build/memberpress-account.css", // src
             [], // dependencies
-            $assets_file["version"] // version
+            null
         );
 
         if($_GET['action'] === 'tab1') {
@@ -221,6 +221,62 @@ if (isset($_GET['update']) && $_GET['update'] === 'true') {
             wp_reset_postdata();
 
           }
+
+         else if($_GET['action'] === 'tab0') {
+
+           // Retrieve the current user's group
+            $current_user_group = get_field('partner_group', 'user_' . get_current_user_id());
+
+            // Retrieve Group Event
+            $group_events = get_field('group_events', $current_user_group[0]->ID);
+            $group_events_ID = array();
+
+            if (!empty($group_events)) {
+                foreach ($group_events as $event) {
+                    $group_events_ID[] = $event->ID;
+                }
+            }
+            $events = new WP_Query(array(
+                'post_type'      => 'event',
+                'post__in'       => $group_events_ID, 
+                'posts_per_page' => -1,
+                'post_status'    => array('publish', 'pending'), // Include both published and pending review posts
+            ));
+        
+            // Display frontend form for editing events
+            if ($events->have_posts()) : ?>
+                <div class="mepr-event-cards">
+            <?php
+                while ($events->have_posts()) : $events->the_post();
+                    ?>
+                    <div class="mepr-event-cards__card">
+                        <h3><?php the_title(); ?></h3>
+                        <h4 class="event-card-status<?php echo get_post_status() === 'publish' ? '--green' : '--red' ?>">Status: <span><?php echo get_post_status(); ?></span></h4>
+                        <div class="event-content">
+                            <?php
+                            // Display trimmed content or excerpt
+                            if (!empty(get_the_excerpt())) {
+                                echo wp_trim_words(get_the_excerpt(), 20); // Display excerpt with maximum of 20 words
+                            } else {
+                                echo wp_trim_words(get_the_content(), 20); // Display content with maximum of 20 words
+                            }
+                            ?>
+                        </div>
+                        <a href="<?php echo esc_url(get_edit_post_link()); ?>" class="mepr-button btn-outline btn btn-outline">Edit Event</a>
+                    </div>
+                    <?php
+                endwhile; ?>
+                </div>
+            <?php
+            else :
+                echo '<p>No events found.</p>';
+            endif;
+            wp_reset_postdata();  
+            ?>  
+            <br>        
+            <a href="<?php echo esc_url(get_edit_post_link()); ?>" class="mepr-button btn">Create New Event</a>
+            <?php
+        }
     }
 
     public static function mepr_enqueue_scripts($is_product_page, $is_group_page, $is_account_page) {
