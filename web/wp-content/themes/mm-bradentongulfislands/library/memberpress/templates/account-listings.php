d4175c80a35189f4446810d461b835531982e5a7
Aaron Fields <afields@maddenmedia.cm>
2024-04-23 07:52:25 -0700

    feat: update listing form

--- web/wp-content/themes/mm-bradentongulfislands/library/memberpress/templates/account-listings.php
+++ web/wp-content/themes/mm-bradentongulfislands/library/memberpress/templates/account-listings.php
@@ -43,7 +43,7 @@ if ($listings->have_posts() && $listings->found_posts === 1) {
             echo '<div style="background-color: #c96a39db; border: 0.5px solid #c96a39; padding: 0.2rem; width:33%;margin-bottom:1rem" class="notice notice-success is-dismissible"><p style="color:white;margin:0;margin-left:1rem;">Listing has been updated successfully.</p></div>';
         }
 
-        include get_stylesheet_directory() . '/library/memberpress/templates/forms/account-event-form.php';
+        include get_stylesheet_directory() . '/library/memberpress/templates/forms/account-listing-form.php';
     
     endwhile;
 }
