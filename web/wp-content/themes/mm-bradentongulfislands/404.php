a02f7488d0e571a18090a2b06afdc49e8754a08c
Aaron Fields <afields@maddenmedia.cm>
2024-07-01 16:30:13 -0700

    404 tweak

--- web/wp-content/themes/mm-bradentongulfislands/404.php
+++ web/wp-content/themes/mm-bradentongulfislands/404.php
@@ -4,7 +4,7 @@
  */
 
 get_header(); ?>
-<div id="content-404 <?php echo isset($_GET['maintenance']) ? 'content-404--maintenance' : ''; ?>" class="page-content">
+<div id="content-404" class="page-content <?php echo isset($_GET['maintenance']) ? 'page-content--maintenance' : ''; ?>">
 	<div class="content-inner">
 		<h1>Oops!<span>Looks like this page<br>went to the beach.</span></h1>
 		<?php if(isset($_GET['maintenance'])) { ?>
