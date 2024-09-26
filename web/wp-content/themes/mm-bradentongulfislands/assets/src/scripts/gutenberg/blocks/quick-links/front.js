3911a85fc200b4083a0d12ba4625112d61bd16be
Aaron Fields <afields@maddenmedia.cm>
2024-09-26 07:12:58 -0700

    quick links

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/quick-links/front.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/quick-links/front.js
@@ -1,61 +1,49 @@
+// this is the front-end script for the block example-dynamic
+
 (function($) {
   $(document).ready(function() {
+      
 
-    function quickLinks() {
 
-      const element = $('.wp-block-mm-bradentongulfislands-quick-links');
-      let isTucked = false;
-      let lastScrollY = $(window).scrollTop();
+    const element = $('.wp-block-mm-bradentongulfislands-quick-links');
+    let isTucked = false;
+    let lastScrollY = $(window).scrollTop();
 
-      // Clear previous scroll and click event handlers to avoid conflicts
-      $(window).off('scroll');
-      element.find('.quick-links-heading').off('click');
 
-      // Desktop behavior
-      if ($(window).width() > 768) {
+    if($(window).width() > 768) {
+      $(window).scroll(function() {
+        const currentScrollY = $(window).scrollTop();
 
-        $(window).scroll(function() {
-          const currentScrollY = $(window).scrollTop();
+        if (currentScrollY > lastScrollY && !isTucked) {
+          // Scrolling down, hide the element
+          element.addClass('tuck');
+          isTucked = true;
+        } else if (currentScrollY < lastScrollY && isTucked) {
+          // Scrolling up, show the element
+          element.removeClass('tuck');
+          isTucked = false;
+        }
 
-          if (currentScrollY > lastScrollY && !isTucked) {
-            // Scrolling down, hide the element
-            element.addClass('tuck');
-            isTucked = true;
-          } else if (currentScrollY < lastScrollY && isTucked) {
-            // Scrolling up, show the element
-            element.removeClass('tuck');
-            isTucked = false;
-          }
+        lastScrollY = currentScrollY;
+      });
 
-          lastScrollY = currentScrollY;
-        });
+    } else {
 
-      } else { 
-        // Mobile behavior
+      element.find('.quick-links-heading').click(function(){
+        element.find('.quick-link-items').slideToggle();
+      });
 
-        element.find('.quick-links-heading').click(function() {
-          element.find('.quick-link-items').slideToggle();
-        });
+      var stickyTop = element.offset().top - 88;
 
-        var stickyTop = element.offset().top - 88;
-
-        $(window).scroll(function() {
-          var windowTop = $(window).scrollTop();
-          if (stickyTop < windowTop) {
-            element.addClass('fixed-scroll');
-          } else {
-            element.removeClass('fixed-scroll');
-          }
-        });
-      }
-
+      $(window).scroll(function() {
+        var windowTop = $(window).scrollTop();
+        if (stickyTop < windowTop) {
+          element.addClass('fixed-scroll');
+        } else {
+          element.removeClass('fixed-scroll');
+        }
+      });
     }
 
-    quickLinks();
-
-    // Listen for window resize and reset the behavior accordingly
-    $(window).resize(function() {
-      quickLinks();
-    });
   });
 })(jQuery);
