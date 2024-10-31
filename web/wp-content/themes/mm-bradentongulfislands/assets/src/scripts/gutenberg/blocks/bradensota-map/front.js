5eac862dd729d32cb52d71624702c41e51115913
Aaron Fields <afields@maddenmedia.cm>
2024-10-31 12:12:52 -0700

    map

---
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/bradensota-map/front.js
@@ -0,0 +1,27 @@
+
+(function($) {
+    $(document).ready(function() {
+        
+     // Place Map
+    $('#bradensotaMap').appendTo('.mapContainer');
+
+    function populateContent(element) {
+        var title = $(element).data('title');
+        var location = $(element).data('location');
+        var description = $(element).data('description');
+    
+        $('.bradensota-map-content__title').text(title);
+        $('.bradensota-map-content__location').text(location);
+        $('.bradensota-map-content__description').text(description);
+      }
+    
+      // Populate content with the first element's data on page load
+      var firstElement = $('.mapContainer image').first();
+      populateContent(firstElement);
+    
+      // Update content on hover over any SVG element
+      $('.mapContainer image').on('mouseenter', function() {
+        populateContent(this);
+      });
+    })
+})(jQuery);
--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/index.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/index.js
@@ -26,13 +26,14 @@ import BeachesMap from './beaches-map';
 import HeroShowcase from './hero-showcase';
 import WaterFerryMap from './water-ferry-map';
 import SocialButton from './social-button';
+import BradensotaMap from './bradensota-map';
 
 /*** CONSTANTS **************************************************************/
 
 // Collect blocks
 // NOTE: ACF blocks should not be registered here, only imported, or they will
 // appear twice in the editor!
-const blocks = [ ContentSlider, Hero, PortraitGrid, WideSlideshow, WideImageSlide, ShowcaseCard, AccordionSection, Accordion, ContentSelector, ContentSection, QuickLinks, UpcomingEvents, ListingsGrid, BeachesMap, HeroShowcase, WaterFerryMap, SocialButton ];
+const blocks = [ ContentSlider, Hero, PortraitGrid, WideSlideshow, WideImageSlide, ShowcaseCard, AccordionSection, Accordion, ContentSelector, ContentSection, QuickLinks, UpcomingEvents, ListingsGrid, BeachesMap, HeroShowcase, WaterFerryMap, BradensotaMap, SocialButton ];
 
 /*** EXPORTS ****************************************************************/
 export default () => {
