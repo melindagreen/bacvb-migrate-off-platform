2f986bbc437f42e4d9c744d388ab74a521fc4ee4
Aaron Fields <afields@maddenmedia.cm>
2025-02-03 02:13:17 -0800

    infographic & polaroid styling

---
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/info-chip/edit.js
@@ -0,0 +1,59 @@
+/*** IMPORTS ****************************************************************/
+
+// WordPress dependencies
+import { } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { RichText, useBlockProps } from '@wordpress/block-editor';
+
+// Local Dependencies
+// Controls - add block/inspector controls here 
+import Controls from './controls'
+
+/*** COMPONTANTS ************************************************************/
+
+/**
+ * The editor for the block
+ * @param {*} props 
+ * @returns {WPElement}
+ */
+
+const Editor = props => {
+  const { attributes: { imageUrl, imageAlt, title }, setAttributes, className } = props;
+  const blockProps = useBlockProps();
+
+  return (
+    <section className={className}>
+      {imageUrl !== "" && 
+      <div className={`${className}__image`}>
+        <img src={imageUrl} data-load-alt={imageAlt !== "" ? imageAlt : "Showcase Card Image"} data-load-type="img" data-load-offset="lg" data-load-all={imageUrl} />
+      </div>
+      }
+      <div className={`${className}__contents`}>
+      <RichText
+        { ...blockProps }
+        tagName="button" 
+        className="contents-title"
+        allowedFormats={ [ "core/bold", "core/italic", "core/link" ] } 
+        value={ title }
+        onChange={ ( title ) => {
+          setAttributes( { title } )
+        } } 
+        placeholder={ __( "Add title..." ) } 
+      />
+      </div>
+    </section>
+  )
+}
+
+const edit = (props) => {
+  return (
+    <>
+      <Controls {...props} />
+      <Editor {...props} />
+    </>
+  );
+};
+
+/*** EXPORTS ****************************************************************/
+
+export default edit;
