c4e6d23d62e7ffec247d7679d9b9f1b59795c93c
Curtis Thompson <cthompson@maddenmedia.com>
2025-03-03 11:34:16 -0700

    new service worker to exclude some pixels from a 3rd party vendor

--- web/service-worker.js
+++ web/service-worker.js
@@ -1,16 +1,21 @@
 // This is the current worker version. Bump if you
 // need to update your cache.
-var version = 1;
+var version = 2;
 
 // These files will be added to the static cache
 // ONLY places files here that are very unlikely to
 // change. A missing file may throw errors and negate
-// the performance benefits of a service woker.
+// the performance benefits of a service worker.
 var toCache = [
     // Images:
     // JS:
     // CSS:
     // Plugin files:
+];
+
+// URLs to exclude from caching
+var excludeCachePatterns = [
+    /https:\/\/px\.adentifi\.com\/Pixels/
 ];
 
 // Cache vital files on install:
@@ -26,50 +31,52 @@ self.addEventListener('install', function(event) {
 
 // Intercept requests and respond from cache if possible:
 self.addEventListener('fetch', function(event) {
+    var requestUrl = event.request.url;
+    
     // Ignore non-GET requests:
     if (event.request.method !== 'GET') {
         return;
     }
+    
+    // Exclude specific URLs from caching:
+    if (excludeCachePatterns.some(pattern => pattern.test(requestUrl))) {
+        return;
+    }
+
     // Open cache:
-    caches.open('mmmadre-dynamic-v' + version)
-    .then(function(cache) {
-        return cache.match(event.request)
-        // If a match exists:
-        .then(function(response) {
-            // Return it and save it:
-            if(response) return response;
-            
-            fetch(event.request)
-            .then(function(response) {
-                if(response.status === 200) cache.put(event.request, response.clone());
-                return response;
-            })
-            .catch(function(error) {
-                console.error('Service worker error: ' + error);
+    event.respondWith(
+        caches.open('mmmadre-dynamic-v' + version).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                // Return from cache if available:
+                if (response) return response;
+                
+                return fetch(event.request).then(function(networkResponse) {
+                    if (networkResponse.status === 200) {
+                        cache.put(event.request, networkResponse.clone());
+                    }
+                    return networkResponse;
+                }).catch(function(error) {
+                    console.error('Service worker error: ' + error);
+                });
             });
         })
-        .catch(function(error) {
-            console.error('Service worker error: ' + error);
-        });
-    })
-    .catch(function(error) {
-        console.error('Service worker error: ' + error);
-    });
+    );
 });
 
 // Clean out old versions:
 self.addEventListener('activate', function(event) {
     event.waitUntil(caches.keys()
     .then(function(cacheNames) {
-        cacheNames.filter(function(cacheName) {
-            return cacheName === 'mmmadre-dynamic-v' + version;
-        })
-        .map(function(cacheName) {
-            console.log('Service worker has cleared outdated caches.');
-            return caches.delete(cacheName);
-        });
+        return Promise.all(
+            cacheNames.filter(function(cacheName) {
+                return cacheName.startsWith('mmmadre-') && cacheName !== 'mmmadre-dynamic-v' + version;
+            }).map(function(cacheName) {
+                console.log('Service worker has cleared outdated caches: ' + cacheName);
+                return caches.delete(cacheName);
+            })
+        );
     })
-    .catch(function(err) {
+    .catch(function(error) {
         console.error('Service worker error: ' + error);
     }));
 });
