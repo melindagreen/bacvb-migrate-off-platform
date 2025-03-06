aa72d950675f764a810fcefda5126ea58fe70c36
Aaron Fields <aaronfields@Aarons-MacBook-Pro.local>
2025-03-05 16:27:25 -0800

    update servicer worker (again)

---
+++ service-worker-v2.js
@@ -0,0 +1,82 @@
+// This is the current worker version. Bump if you
+// need to update your cache.
+var version = 2;
+
+// These files will be added to the static cache
+// ONLY places files here that are very unlikely to
+// change. A missing file may throw errors and negate
+// the performance benefits of a service worker.
+var toCache = [
+    // Images:
+    // JS:
+    // CSS:
+    // Plugin files:
+];
+
+// URLs to exclude from caching
+var excludeCachePatterns = [
+    /https:\/\/px\.adentifi\.com\/Pixels/
+];
+
+// Cache vital files on install:
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open('mmmadre-core-v' + version)
+    .then(function(cache) {
+        return cache.addAll(toCache);
+    })
+    .catch(function(error) {
+        console.error('Service worker error: ' + error);
+    }));
+});
+
+// Intercept requests and respond from cache if possible:
+self.addEventListener('fetch', function(event) {
+    var requestUrl = event.request.url;
+    
+    // Ignore non-GET requests:
+    if (event.request.method !== 'GET') {
+        return;
+    }
+    
+    // Exclude specific URLs from caching:
+    if (excludeCachePatterns.some(pattern => pattern.test(requestUrl))) {
+        return;
+    }
+
+    // Open cache:
+    event.respondWith(
+        caches.open('mmmadre-dynamic-v' + version).then(function(cache) {
+            return cache.match(event.request).then(function(response) {
+                // Return from cache if available:
+                if (response) return response;
+                
+                return fetch(event.request).then(function(networkResponse) {
+                    if (networkResponse.status === 200) {
+                        cache.put(event.request, networkResponse.clone());
+                    }
+                    return networkResponse;
+                }).catch(function(error) {
+                    console.error('Service worker error: ' + error);
+                });
+            });
+        })
+    );
+});
+
+// Clean out old versions:
+self.addEventListener('activate', function(event) {
+    event.waitUntil(caches.keys()
+    .then(function(cacheNames) {
+        return Promise.all(
+            cacheNames.filter(function(cacheName) {
+                return cacheName.startsWith('mmmadre-') && cacheName !== 'mmmadre-dynamic-v' + version;
+            }).map(function(cacheName) {
+                console.log('Service worker has cleared outdated caches: ' + cacheName);
+                return caches.delete(cacheName);
+            })
+        );
+    })
+    .catch(function(error) {
+        console.error('Service worker error: ' + error);
+    }));
+});
--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,75 @@
-// Increment this version to force cache updates
-var version = 3;
+// This is the current worker version. Bump if you
+// need to update your cache.
+var version = 1;
 
-// Files to cache (static assets only)
+// These files will be added to the static cache
+// ONLY places files here that are very unlikely to
+// change. A missing file may throw errors and negate
+// the performance benefits of a service woker.
 var toCache = [
-    // Add files that should be cached
+    // Images:
+    // JS:
+    // CSS:
+    // Plugin files:
 ];
 
-// URLs to exclude from caching
-var excludeCachePatterns = [
-    /https:\/\/px\.adentifi\.com\/Pixels/,
-    /\/wp-admin/,
-    /\/wp-json/
-];
-
-// Cache static files on install:
+// Cache vital files on install:
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('mmmadre-core-v' + version).then(function(cache) {
-            return cache.addAll(toCache);
-        }).catch(function(error) {
-            console.error('Service worker install error:', error);
-        })
-    );
+    event.waitUntil(caches.open('mmmadre-core-v' + version)
+    .then(function(cache) {
+        return cache.addAll(toCache);
+    })
+    .catch(function(error) {
+        console.error('Service worker error: ' + error);
+    }));
 });
 
-// Intercept fetch requests:
+// Intercept requests and respond from cache if possible:
 self.addEventListener('fetch', function(event) {
-    var requestUrl = event.request.url;
-
     // Ignore non-GET requests:
     if (event.request.method !== 'GET') {
         return;
     }
-
-    // Bypass cache for excluded URLs (admin, REST API, etc.)
-    if (excludeCachePatterns.some(pattern => pattern.test(requestUrl))) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-
-    event.respondWith(
-        caches.open('mmmadre-dynamic-v' + version).then(function(cache) {
-            return fetch(event.request).then(function(networkResponse) {
-                if (networkResponse.status === 200) {
-                    cache.put(event.request, networkResponse.clone());
-                }
-                return networkResponse;
-            }).catch(function() {
-                return cache.match(event.request) || fetch(event.request);
+    // Open cache:
+    caches.open('mmmadre-dynamic-v' + version)
+    .then(function(cache) {
+        return cache.match(event.request)
+        // If a match exists:
+        .then(function(response) {
+            // Return it and save it:
+            if(response) return response;
+            
+            fetch(event.request)
+            .then(function(response) {
+                if(response.status === 200) cache.put(event.request, response.clone());
+                return response;
+            })
+            .catch(function(error) {
+                console.error('Service worker error: ' + error);
             });
         })
-    );
+        .catch(function(error) {
+            console.error('Service worker error: ' + error);
+        });
+    })
+    .catch(function(error) {
+        console.error('Service worker error: ' + error);
+    });
 });
 
-// Clean old caches on activation:
+// Clean out old versions:
 self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(function(cacheName) {
-                    return cacheName.startsWith('mmmadre-') && !cacheName.includes(version);
-                }).map(function(cacheName) {
-                    console.log('Deleting old cache:', cacheName);
-                    return caches.delete(cacheName);
-                })
-            );
+    event.waitUntil(caches.keys()
+    .then(function(cacheNames) {
+        cacheNames.filter(function(cacheName) {
+            return cacheName === 'mmmadre-dynamic-v' + version;
         })
-    );
+        .map(function(cacheName) {
+            console.log('Service worker has cleared outdated caches.');
+            return caches.delete(cacheName);
+        });
+    })
+    .catch(function(err) {
+        console.error('Service worker error: ' + error);
+    }));
 });
