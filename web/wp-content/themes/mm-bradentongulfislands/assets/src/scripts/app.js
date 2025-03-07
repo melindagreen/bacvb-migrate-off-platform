a7e284ae79f3bfd7d78c59b2b74580441168d5ea
Aaron Fields <aaronfields@Aarons-MacBook-Pro.local>
2025-03-07 10:43:28 -0800

    disable service worker

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/app.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/app.js
@@ -16,14 +16,14 @@ if ('serviceWorker' in navigator) {
 		} 
 	});
 
-    navigator.serviceWorker.register('/service-worker.js')
-        .then((reg) => {
-            // registration worked
-            console.log('Registration succeeded. Scope is ' + reg.scope);
-        }).catch((error) => {
-            // registration failed
-            console.log('Registration failed with ' + error);
-        });
+    // navigator.serviceWorker.register('/service-worker.js')
+    //     .then((reg) => {
+    //         // registration worked
+    //         console.log('Registration succeeded. Scope is ' + reg.scope);
+    //     }).catch((error) => {
+    //         // registration failed
+    //         console.log('Registration failed with ' + error);
+    //     });
 }
 
 (function ($) {
