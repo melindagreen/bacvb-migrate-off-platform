c189cd9a6a964a6b1bc85ee267cdcaa577ee9ed2
Aaron Fields <aaronfields@Aarons-MacBook-Pro.local>
2025-03-19 09:08:33 -0700

    content slider adjustment

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/content-slider/render.php
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/content-slider/render.php
@@ -10,13 +10,14 @@ use MaddenNino\Library\Constants as Constants;
  */
 function render_block( $attrs, $content ) {
   $html = "<section class='mm-content-slider'>
-    <div class='swiper-wrapper'>".$content."</div>
-    <div class='temp-holder-pagination'></div>
-    <div class='content-slider-controls'>
+    <div class='swiper-wrapper'>".$content."
+        <div class='content-slider-controls'>
     <div class='content-slider-swiper-button-prev'><img src='".get_stylesheet_directory_uri()."/assets/images/prev.png' /></div>
     <div class='content-sliderswiper-pagination'></div>
     <div class='content-slider-swiper-button-next'><img src='".get_stylesheet_directory_uri()."/assets/images/next.png' /></div>
     </div>
+    </div>
+    <div class='temp-holder-pagination'></div>
   </section>";
   return $html;
 }
