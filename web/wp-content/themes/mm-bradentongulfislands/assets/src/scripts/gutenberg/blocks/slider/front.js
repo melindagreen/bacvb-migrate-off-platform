4cc90b51ac7eeb47db7907af36ad3a7278eab680
Aaron Fields <aaronfields@Aarons-MacBook-Pro.local>
2025-04-04 14:07:46 -0700

    remove block settting

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/slider/front.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/slider/front.js
@@ -59,9 +59,9 @@
               centeredSlides: slider.dataset.centeredslides ? true : false,
               effect: slider.dataset.effect ?? 'slide',
               loop: slider.dataset.loop ? true : false,
-              loopPreventsSliding: false,
-              loopAdditionalSlides: 1,
-              loopAddBlankSlides: true,
+              // loopPreventsSliding: false,
+              // loopAdditionalSlides: 1,
+              // loopAddBlankSlides: true,
               freeMode: {
                 enabled: slider.dataset.freemode ? true : false,
               },
