a3a33756ed999b664ee987001ef236f364348b40
Aaron Fields <aaronfields@Aarons-MacBook-Pro.local>
2025-05-02 16:34:35 -0700

    ferry map issue

--- web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/water-ferry-map/assets/water-ferry-map.js
+++ web/wp-content/themes/mm-bradentongulfislands/assets/src/scripts/gutenberg/blocks/water-ferry-map/assets/water-ferry-map.js
@@ -6,34 +6,35 @@ $(window).on("load", () => {
 
 export const initInteractiveMap = () => {
 
-    console.log("Initializing interactive map...");
-
+   
     $('.wp-block-mm-bradentongulfislands-water-ferry-map svg .water-ferry-stop').on('click', function(event) {
-        console.log("Water ferry stop clicked.");
+        console.log('Water ferry stop clicked.');
 
         var stopId = $(this).attr('id');
-        console.log("Stop ID:", stopId);
+        console.log('Stop ID:', stopId);
 
         // Hide all city cards 
         $('.ferry-stop-card').each(function() {
-            console.log("Hiding city card:", $(this).attr('class'));
+            console.log('Hiding card:', $(this).attr('class'));
             $(this).removeClass('pop-in').addClass('pop-out');
         });
 
         // Show the city card 
         let stopCard = $('.ferry-stop-card.' + stopId);
-        console.log("Showing city card for stop ID:", stopId);
+        console.log('Showing card for stop ID:', stopId);
         stopCard.removeClass('pop-out').addClass('pop-in');
+        $('.ferry-stop-lightbox').addClass('ferry-stop-lightbox--on');
+        console.log('Lightbox turned on.');
     });
 
     // Close on click
-    $('.wp-block-mm-bradentongulfislands-water-ferry-map .ferry-stop-card .close').on('click', function() {
-        console.log("Close button clicked.");
+    $('.ferry-stop-lightbox .close').on('click', function() {
+        console.log('Close button clicked.');
 
         var $cityCard = $(this).closest('.ferry-stop-card');
-        console.log("Hiding city card:", $cityCard.attr('class'));
+        console.log('Closing card:', $cityCard.attr('class'));
         $cityCard.removeClass('pop-in').addClass('pop-out');
+        $('.ferry-stop-lightbox').removeClass('ferry-stop-lightbox--on');
+        console.log('Lightbox turned off.');
     });
-
-    console.log("Interactive map initialized.");
 }
