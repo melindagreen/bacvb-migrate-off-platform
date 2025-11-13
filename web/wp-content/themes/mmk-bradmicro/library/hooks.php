b0b580caef19888b4df8c721fb0438d83c29e045
Joe Anderson <janderson@maddenmedia.com>
2025-11-13 09:30:47 -0600

    Added GTM tags

--- web/wp-content/themes/mmk-bradmicro/library/hooks.php
+++ web/wp-content/themes/mmk-bradmicro/library/hooks.php
@@ -18,14 +18,14 @@ add_action( 'wp', __NAMESPACE__ . '\find_content' );
  */
 function header_snippets() {
     ?>
-    <link rel="stylesheet" href="https://use.typekit.net/vww5ibj.css">
-    <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-XXXXXXX');</script> -->
-    <!-- End Google Tag Manager -->
+<link rel="stylesheet" href="https://use.typekit.net/vww5ibj.css">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MZ8H2VT');</script>
+<!-- End Google Tag Manager -->
     <?php
 }
 
@@ -35,8 +35,8 @@ function header_snippets() {
 function body_snippets() {
   ?>
 <!-- Google Tag Manager (noscript) -->
-<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ8H2VT"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
   <?php
 }
